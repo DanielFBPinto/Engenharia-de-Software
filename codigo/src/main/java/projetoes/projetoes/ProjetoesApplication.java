9a88f2b536ca861ea4c2746c0bb0b6d33778fab8
João <35556@ufp.edu.pt>
2018-11-21 10:51:04 +0000

    intelij project 2

--- codigo/src/main/java/projetoes/projetoes/ProjetoesApplication.java
+++ codigo/src/main/java/projetoes/projetoes/ProjetoesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjetoesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoesApplication.class, args);
+    }
 }
