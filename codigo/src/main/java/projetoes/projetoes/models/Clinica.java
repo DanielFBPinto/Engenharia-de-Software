9afad295d2f0dfca6d41e0280ee1f40a73e21b13
João <35556@ufp.edu.pt>
2018-12-31 17:11:46 +0000

    CLINICA DE PUTAS

---
+++ codigo/src/main/java/projetoes/projetoes/models/Clinica.java
@@ -0,0 +1,11 @@
+package projetoes.projetoes.models;
+
+public class Clinica
+{
+    private String localizacao;
+
+    public Clinica(String localizacao)
+    {
+        this.localizacao = localizacao;
+    }
+}
